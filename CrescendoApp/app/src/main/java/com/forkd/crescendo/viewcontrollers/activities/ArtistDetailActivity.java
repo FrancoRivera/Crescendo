30f160d992ef5791cf7df5b2598ec3cdb966887f
Roosevelt <panta_85.x@hotmail.com>
2018-05-02 17:21:04 -0500

    added artist detail activity

---
+++ CrescendoApp/app/src/main/java/com/forkd/crescendo/viewcontrollers/activities/ArtistDetailActivity.java
@@ -0,0 +1,23 @@
+package com.forkd.crescendo.viewcontrollers.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.forkd.crescendo.R;
+
+public class ArtistDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
