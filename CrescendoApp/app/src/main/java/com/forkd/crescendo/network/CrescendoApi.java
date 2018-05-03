0260e32e77dbf13226770705ff70aaad715333be
Roosevelt <panta_85.x@hotmail.com>
2018-05-02 21:21:25 -0500

    login validation

---
+++ CrescendoApp/app/src/main/java/com/forkd/crescendo/network/CrescendoApi.java
@@ -0,0 +1,9 @@
+package com.forkd.crescendo.network;
+
+public class CrescendoApi {
+    private static String BASE_URL = "https://crescendo-api-tirwrpcnoo.now.sh/";
+
+    public static String authentication() {
+        return BASE_URL + "authentication";
+    }
+}
